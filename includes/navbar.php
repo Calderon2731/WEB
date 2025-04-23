a5e5bf9bf623960301f324cf57094cf6ca9408bc
Kendall Calderon Acuña <calderonacunakendall@gmail.com>
2025-04-22 20:30:01 -0600

    haciendo un navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,23 @@
 <html>
 <body>
     <nav class = "container-main">
-        
+        <ul class = "clase-Navbar">
+            <li class = "lis-ordenada">
+                <a href="#" class ="classa">inicio</a>
+            </li>
+            
+            <li class = "lis-ordenada">
+                <a href="#" class ="classa">sobre nosotros</a>  
+            </li>
+
+            <li class = "lis-ordenada">
+                <a href="#" class ="classa">accesorios</a>  
+            </li>
+
+            <li class = "lis-ordenada">
+                <a href="#" class ="classa">domicilio</a>  
+            </li>
+        </ul>
     </nav>
 </body>
 
--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Web</title>
     <?php 
-    include ('../includes/navbar.php');
-    
+    include ('./includes/head.php');
+    include ('./includes/navbar.php');  
     ?>
-   
 </head>
 
 <body>
-    <h1>pagina</h1>
-    <h2>en este repositorio se ve practicas</h2>
+
+
 
 </body>
 </html>
